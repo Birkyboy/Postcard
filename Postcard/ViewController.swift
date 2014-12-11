f2f14241251580e77d8a7c3d57eabc89d8457042
birkyboy <birkyboy@MacBook-Pro.local>
2014-12-11 20:09:20 +0100

    Second commit addes a comment to test commits

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -26,6 +26,7 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        // Adding a comments to test commits on github.
     }
 
     @IBAction func sendMailButtonPressed(sender: UIButton) {
